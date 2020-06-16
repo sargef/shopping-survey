cf0f2e416bac05eba45210c8c6cd2083a1abec49
sargef <sargef38@bigpond.com>
2020-06-16 13:01:43 +0930

    edited styles

--- src/VoteForm.js
+++ src/VoteForm.js
@@ -222,7 +222,7 @@ class VoteForm extends Component{
     inputChangeHandler = (event, controlName) => {
         this.setState({validationState: null});
         let array;
-        if (controlName === 'question1'){
+        if (controlName === 'question2'){
           array = this.state.controls[controlName].value;
           array.indexOf(event.target.value) === -1? array.push(event.target.value):array.splice(array.indexOf(event.target.value), 1);
         }
@@ -230,7 +230,7 @@ class VoteForm extends Component{
             ...this.state.controls,
             [controlName]: {
                 ...this.state.controls[controlName],
-                value: controlName === 'question1'? array : event.target.value,
+                value: controlName === 'question2'? array : event.target.value,
                 valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
             }
@@ -243,8 +243,8 @@ class VoteForm extends Component{
       const target = event.target;
       let value = target.type === 'checkbox' ? target.checked : target.value;
       const name = target.name;
-      if (name === 'question1'){
-        value = this.state.question1;
+      if (name === 'question2'){
+        value = this.state.question2;
         value.indexOf(target.value) === -1? value.push(target.value):value.splice(value.indexOf(target.value), 1);
       }
   
